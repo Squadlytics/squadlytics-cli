352b1e3ce1a85c1e0a9891a5922162ef079e223a
Sten Pittet <sten@squadlytics.com>
2017-11-29 17:00:31 +0000

    Fixed the deployment notification script.

--- squadlytics-rec-deployment.js
+++ squadlytics-rec-deployment.js
@@ -3,17 +3,28 @@ program = require('commander');
 axios = require('axios');
 
 const source = 'squadlytics-cli';
-let event, application_name, author_email, commit_hash, description, notification_url;
+let   event,
+      deployment_id,
+      application_name,
+      author_email,
+      status,
+      commit_hash,
+      description,
+      notification_url;
 
 program
   .arguments('<notification_url>')
   .option('-a, --application <application>', 'Name of the application you are deploying')
+  .option('-i, --id <deployment_id>', 'Unique id for your deployment')
   .option('-u, --user [email]', 'Optional email of the user associated with the deployment')
+  .option('-s, --status [status]', 'Optional deployment status')
   .option('-c, --commit [commit]', 'Optional commit being deployed')
   .option('-d, --message [message]', 'Optional description of the deployment')
   .action((notificationURL) => {
     application_name = program.application;
+    deployment_id = program.id
     author_email = program.user;
+    status = program.status || 'SUCCESSFUL';
     commit_hash = program.commit;
     description = program.message;
     notification_url = notificationURL;
@@ -32,10 +43,18 @@ if (!application_name){
   process.exit(1);
 }
 
+if (!deployment_id){
+  console.error('Deployment ID is missing.');
+  console.error(program.usage());
+  process.exit(1);
+}
+
 const body = {
   source,
   application_name,
+  deployment_id,
   author_email,
+  status,
   commit_hash,
   description
 };
@@ -50,15 +69,14 @@ const headers = {
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 axios
-  .post(notification_url, body, headers)
+  .post(notification_url, body, { headers })
   .catch((error) => {
     if (error.response) {
       // The request was made and the server responded with a status code
       // that falls out of the range of 2xx
-      console.error(error);
+      console.error(error.response.data);
     } else {
       // Something happened in setting up the request that triggered an Error
       console.error('Error', error.message);
-      console.error(error)
     }
   });
